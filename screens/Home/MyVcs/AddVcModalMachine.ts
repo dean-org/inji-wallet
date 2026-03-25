367fba50b8287fa6f9b1a9ec9c28ecab15310d9f
Dharanidharan-S-04 <dean12345.dd@gmail.com>
2026-03-25 20:04:47 +0530

    Update AddVcModalMachine.ts
    Signed-off-by: Dharanidharan-S-04 <dean12345.dd@gmail.com>

--- screens/Home/MyVcs/AddVcModalMachine.ts
+++ screens/Home/MyVcs/AddVcModalMachine.ts
@@ -253,7 +253,10 @@ export const AddVcModalMachine =
         },
         done: {
           type: 'final',
-          data: context => new VCMetadata(context),
+          data: context => new VCMetadata(...context,
+          id: context.idType === 'UID' ? `${context.id}@uid` : context.id,
+          idType: context.idType === 'UID' ? 'HANDLE' : context.idType,
+        }),
         },
       },
     },
